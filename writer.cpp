9d0f6a718ac7e1cfdcd6a2ed393edcdc131363b6
Y00-J <83537524+Y00-J@users.noreply.github.com>
2023-01-03 20:53:47 +0900

    Add files via upload

--- writer.cpp
+++ writer.cpp
@@ -17,17 +17,19 @@ struct check_data
 int main()
 {
     int shmid;
-    struct shmid_ds shm_stat;
+    char buffer[SIZE];
     void* shmaddr;
 
-    // 1234 키의 공유메모리 있으면 접근해서 식별자 얻음
-    if ((shmid = shmget((key_t)1234, SIZE, IPC_CREAT | 0666)) == -1) 
+
+
+    // 1234 키의 공유메모리 생성
+    if ((shmid = shmget((key_t)1234, sizeof(struct check_data), IPC_CREAT | 0666)) == -1) 
     {
         perror("shmid failed");
         exit(1);
     }
 
-    // shmid 공유메모리를 호출 프로세스 메모리 영역으로 첨부
+    // shmid 공유메모리를 호출 프로세스 메모리 영역으로 첨부 */
     if ((shmaddr = shmat(shmid, (void*)0, 0)) == (void*)-1)
     {
         perror("shmat failed");
@@ -36,38 +38,26 @@ int main()
 
     shared_data = (struct check_data*)shmaddr;
 
-    // check가 0이면 공유메모리에 도착한 데이터가 없다는 의미
-    shared_data->check = 0;
+    while (1)
+    {
+        printf("input data ==> ");
+        fgets(buffer, SIZE, stdin);
+        strncpy(shared_data->data, buffer, SIZE); // 공유메모리에 데이터 쓰기
+        shared_data->check = 1;
 
-    while (1) {
-
-        /* 공유메모리에 데이터 도착하면 */
-
-        if (shared_data->check) 
-        {
-            printf("data read from shared memory : %s", shared_data->data);
-            shared_data->check = 0;
-
-            /* 읽은 데이터가 ‘q’이면 */
+	    /* 쓴 데이터가 ‘q’이면 */
             if (!strncmp(shared_data->data, "q", 1)) 
             {
                  break;
             }
-        }
+	
     }
+
     // 공유메모리를 호출 프로세스의 메모리 영역에서 분리
 
     if (shmdt(shmaddr) == -1) 
     {
         perror("shmdt failed");
-        exit(1);
-    }
-
-    // shmid 공유메모리 삭제
-
-    if (shmctl(shmid, IPC_RMID, 0) == -1) 
-    {
-        perror("shmctl failed");
         exit(1);
     }
 
